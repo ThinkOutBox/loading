1757f6b4b15e26c2436f3c7095ed17bbbce1066d
ThinkOutBox <mcnm707@gmail.com>
2025-07-28 14:28:28 +0300

    Update task_1.dart
    done

--- 1_dart_concepts/1_1_mixins/task_1.dart
+++ 1_dart_concepts/1_1_mixins/task_1.dart
@@ -1,4 +1,20 @@
 void main() {
+  final now = DateTime.now();
+  print(now.formatYMDHM());
   // Implement an extension on [DateTime], returning a [String] in format of
   // `YYYY.MM.DD hh:mm:ss` (e.g. `2023.01.01 00:00:00`).
+}
+
+extenstion FormatDateTime on DateTime {
+  String formatYMDHM() {
+    String twoDigits(int n) => n.toString().padLeft(2,'0');
+
+    final year = this.year;
+    final month = twoDigits(this.month);
+    final day = twoDigits(this.day);
+    final hour = twoDigits(this.hour);
+    final minute = twoDigits(this.minute);
+
+    return '$year.$month.$day.$hour.$minute';
+  }
 }
