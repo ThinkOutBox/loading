74987733dc94a227a5eeb812f03df1b737427685
ThinkOutBox <mcnm707@gmail.com>
2025-07-31 11:20:48 +0300

    Update task_2.dart
    done

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,7 +1,61 @@
 void main() {
-  // Implement an extension on [String], parsing links from a text.
-  //
-  // Extension should return a [List] of texts and links, e.g.:
-  // - `Hello, google.com, yay` ->
-  //   [Text('Hello, '), Link('google.com'), Text(', yay')].
+  final input = 'Hello, check google.com and also https://dart.dev!';
+  final parts = input.extractLinks();
+
+  for (final part in parts) {
+    if (part is LinkPart) {
+      print('Link: ${part.url}');
+    } else if (part is TextPart) {
+      print('Text: ${part.content}');
+    }
+  }
+}
+
+/// Базовый класс для частей текста
+abstract class TextPart {}
+
+/// Обычный текст
+class TextPartImpl extends TextPart {
+  final String content;
+  TextPartImpl(this.content);
+}
+
+/// Ссылка
+class LinkPart extends TextPart {
+  final String url;
+  LinkPart(this.url);
+}
+
+/// Расширение для String
+extension LinkParser on String {
+  /// Метод извлекает ссылки и обычный текст в виде списка TextPart
+  List<TextPart> extractLinks() {
+    final linkRegex = RegExp(
+      r'((https?:\/\/)?([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/\S*)?)',
+      caseSensitive: false,
+    );
+
+    final matches = linkRegex.allMatches(this);
+    final parts = <TextPart>[];
+
+    int lastIndex = 0;
+
+    for (final match in matches) {
+      if (match.start > lastIndex) {
+        // Добавляем текст между ссылками
+        parts.add(TextPartImpl(substring(lastIndex, match.start)));
+      }
+
+      final url = match.group(0)!;
+      parts.add(LinkPart(url));
+      lastIndex = match.end;
+    }
+
+    if (lastIndex < length) {
+      // Добавляем оставшийся текст
+      parts.add(TextPartImpl(substring(lastIndex)));
+    }
+
+    return parts;
+  }
 }
